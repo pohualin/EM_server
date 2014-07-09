96609059e6d443391e8a52033f1b9ec9d4133fef
Matt Fleming <7wjchnb836@snkmail.com>
2014-07-09 17:38:41 -0500

    mods

--- domain/src/main/java/com/emmisolutions/emmimanager/model/SalesforceAccount.java
+++
@@ -1,7 +0,0 @@
-package com.emmisolutions.emmimanager.model;
-
-/**
- * Created by matt on 7/7/14.
- */
-public class SalesforceAccount {
-}
--- web-api/src/main/java/com/emmisolutions/emmimanager/api/ClientAPI.java
+++ web-api/src/main/java/com/emmisolutions/emmimanager/api/ClientAPI.java
@@ -1,7 +1,15 @@
 package com.emmisolutions.emmimanager.api;
 
+import com.emmisolutions.emmimanager.model.Client;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 /**
- * Created by matt on 7/9/14.
+ * Client API accessible from the web.
  */
-public interface ClientAPI {
+public interface ClientApi {
+
+    Page<Client> list(Pageable pageable, String nameFilter, String statusFilter);
+
+    Client get(Client client);
 }
