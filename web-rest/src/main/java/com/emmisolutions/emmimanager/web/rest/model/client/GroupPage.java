4cb37b8091ac88d49e02ee2047fa001a48cde49a
Amrita Chavan <achavan@emmisolutions.com>
2014-08-20 11:51:16 -0500

    EM-40: hateous wrapper for Groups

---
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/model/client/GroupPage.java
@@ -0,0 +1,70 @@
+package com.emmisolutions.emmimanager.web.rest.model.client;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.TemplateVariable;
+import org.springframework.hateoas.TemplateVariables;
+import org.springframework.hateoas.UriTemplate;
+
+import com.emmisolutions.emmimanager.model.Group;
+import com.emmisolutions.emmimanager.model.GroupSearchFilter;
+import com.emmisolutions.emmimanager.web.rest.model.PagedResource;
+import com.emmisolutions.emmimanager.web.rest.resource.GroupsResource;
+
+/**
+ * A HATEOAS wrapper for a page of GroupResource objects.
+ */
+@XmlRootElement(name = "group-page")
+public class GroupPage extends PagedResource<GroupResource> {
+
+    @XmlElement(name = "filter")
+    private GroupSearchFilter searchFilter;
+
+    public GroupPage() {
+    }
+
+    /**
+     * Wrapper for group resource objects
+     *
+     * @param groupResourceSupports to be wrapped
+     * @param groupPage             true page
+     * @param filter                 which caused the response
+     */
+    public GroupPage(PagedResources<GroupResource> groupResourceSupports, Page<Group> groupPage, GroupSearchFilter filter) {
+        pageDefaults(groupResourceSupports, groupPage);
+        addFilterToLinks(filter);
+    }
+
+    /**
+     * Create the search link
+     *
+     * @return Link for group searches
+     * @see com.emmisolutions.emmimanager.web.rest.resource.GroupsResource#list(org.springframework.data.domain.Pageable, org.springframework.data.domain.Sort, String, org.springframework.data.web.PagedResourcesAssembler, String...)
+     */
+    public static Link createFullSearchLink() {
+        Link link = linkTo(methodOn(GroupsResource.class).listGroupsByClientID(null, null, null, (Long) null)).withRel("group");
+        UriTemplate uriTemplate = new UriTemplate(link.getHref())
+                .with(new TemplateVariables(
+                        new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                        new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED),
+                        new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED),
+                        new TemplateVariable("name", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED),
+                        new TemplateVariable("status", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED)
+                        ));
+        return new Link(uriTemplate, link.getRel());
+    }
+    
+
+    private void addFilterToLinks(GroupSearchFilter filter) {
+        this.searchFilter = filter;
+    }
+
+    
+}
--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/resource/GroupsResource.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/resource/GroupsResource.java
@@ -33,113 +33,101 @@ import com.emmisolutions.emmimanager.web.rest.model.client.GroupResourceAssemble
  */
 
 @RestController
-@RequestMapping(value = "/webapi",
-        produces = {APPLICATION_JSON_VALUE, APPLICATION_XML_VALUE}
-)
+@RequestMapping(value = "/webapi", produces = { APPLICATION_JSON_VALUE,
+		APPLICATION_XML_VALUE })
 public class GroupsResource {
 
-    @Resource
-    GroupService groupService;
+	@Resource
+	GroupService groupService;
 
-    @Resource
-    GroupResourceAssembler groupResourceAssembler;
+	@Resource
+	GroupResourceAssembler groupResourceAssembler;
 
+	/**
+	 * GET to search for groups by client id
+	 *
+	 * @param pageable paged request
+	 * @param sort sorting request
+	 * @param status to filter by
+	 * @param assembler used to create the PagedResources
+	 * @param names to filter by
+	 * @return GroupPage or NO_CONTENT
+	 */
+	@RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.GET)
+	@RolesAllowed({ "PERM_GOD", "PERM_CLIENT_LIST" })
+	public ResponseEntity<GroupPage> listGroupsByClientID(
+			@PageableDefault(size = 50) Pageable pageable,
+			@SortDefault(sort = "id") Sort sort,
+			PagedResourcesAssembler<Group> assembler,
+			@PathVariable("clientId") Long clientId) {
 
-    /**
-     * GET to search for groups by client id
-     *
-     * @param pageable  paged request
-     * @param sort      sorting request
-     * @param status    to filter by
-     * @param assembler used to create the PagedResources
-     * @param names     to filter by
-     * @return GroupPage or NO_CONTENT
-     */
-    @RequestMapping(value = "/clients/{clientId}/groups",
-            method = RequestMethod.GET)
-    @RolesAllowed({"PERM_GOD", "PERM_CLIENT_LIST"})
-    public ResponseEntity<GroupPage> listGroupsByClientID(
-            @PageableDefault(size = 50) Pageable pageable,
-            @SortDefault(sort = "id") Sort sort,
-            PagedResourcesAssembler<Group> assembler,
-            @RequestParam(value = "clientId") Long clientId) {
+		GroupSearchFilter groupSearchFilter = new GroupSearchFilter(clientId);
+		Page<Group> groupPage = groupService.list(pageable, groupSearchFilter);
 
-        GroupSearchFilter groupSearchFilter = new GroupSearchFilter(clientId);
-        Page<Group> groupPage = groupService.list(pageable, groupSearchFilter);
+		if (groupPage.hasContent()) {
+			return new ResponseEntity<>(new GroupPage(assembler.toResource(groupPage, groupResourceAssembler), groupPage, groupSearchFilter), HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		}
+	}
 
-        if (groupPage.hasContent()) {
-            return new ResponseEntity<>(
-                    new GroupPage(assembler.toResource(groupPage, groupResourceAssembler), groupPage, groupSearchFilter),
-                    HttpStatus.OK
-            );
-        } else {
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
-    }
-    
-    /**
-     * POST to create new group
-     *
-     * @param group 		to create
-     * @param clientId 		ID of the client to associate group with
-     * @return GroupResource or INTERNAL_SERVER_ERROR if it could not be created
-     */
-    @RequestMapping(value = "/clients/{clientId}/groups",
-            method = RequestMethod.POST,
-            consumes = {APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE}
-    )
-    @RolesAllowed({"PERM_GOD", "PERM_CLIENT_CREATE"})
-    public ResponseEntity<GroupResource> create(@RequestBody Group group, 
-    		@RequestParam(value = "clientId") Long clientId) {
+	/**
+	 * POST to create new group
+	 *
+	 * @param group  		to create
+	 * @param clientId 		ID of the client to associate group to
+	 * @return GroupResource or INTERNAL_SERVER_ERROR if it could not be created
+	 */
+	@RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.POST, consumes = {
+			APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE })
+	@RolesAllowed({ "PERM_GOD", "PERM_CLIENT_CREATE" })
+	public ResponseEntity<GroupResource> create(@RequestBody Group group,
+			@PathVariable("clientId") Long clientId) {
 
-    	group = groupService.create(group, clientId);
+		group = groupService.create(group, clientId);
 
-        if (group == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } else {
-            return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.CREATED);
-        }
-    } 
-    
-    /**
-     * PUT to update the group
-     *
-     * @param group to update
-     * @return GroupResource or INTERNAL_SERVER_ERROR if update were unsuccessful
-     */
-    @RequestMapping(value = "/clients/{clientId}/groups",
-            method = RequestMethod.PUT,
-            consumes = {APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE}
-    )
-    @RolesAllowed({"PERM_GOD", "PERM_CLIENT_EDIT"})
-    public ResponseEntity<GroupResource> update(@RequestBody Group group) {
-        group = groupService.update(group);
-        if (group == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } else {
-            return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
-        }
-    }
-    
-    /**
-     * GET To get group by id
-     * 
-     * @param id to load
-     * @return GroupResource or INTERNAL_SERVER_ERROR on fail
-     *
-     */
-    @RequestMapping(value = "/groups/{id}",
-            method = RequestMethod.GET,
-            consumes = {APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE}
-    )
-    @RolesAllowed({"PERM_GOD", "PERM_CLIENT_EDIT"})
-    public ResponseEntity<GroupResource> getGroupById(@PathVariable("id") Long id) {
-        Group group = groupService.reload(id);
-        if (group == null) {
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        } else {
-            return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
-        }
-    }
-    
+		if (group == null) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} else {
+			return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.CREATED);
+		}
+	}
+
+	/**
+	 * PUT to update the group
+	 *
+	 * @param group to update
+	 * @return GroupResource or INTERNAL_SERVER_ERROR if update were
+	 *         unsuccessful
+	 */
+	@RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.PUT, consumes = {
+			APPLICATION_XML_VALUE, APPLICATION_JSON_VALUE })
+	@RolesAllowed({ "PERM_GOD", "PERM_CLIENT_EDIT" })
+	public ResponseEntity<GroupResource> update(@RequestBody Group group) {
+		group = groupService.update(group);
+		if (group == null) {
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} else {
+			return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
+		}
+	}
+
+	/**
+	 * GET To get group by id
+	 * 
+	 * @param id 	to load
+	 * @return GroupResource or NO_CONTENT on fail
+	 *
+	 */
+	@RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
+	@RolesAllowed({ "PERM_GOD", "PERM_CLIENT_EDIT" })
+	public ResponseEntity<GroupResource> getGroupById(@PathVariable("id") Long id) {
+		Group group = groupService.reload(id);
+		if (group == null) {
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+		} else {
+			return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
+		}
+	}
+
 }
