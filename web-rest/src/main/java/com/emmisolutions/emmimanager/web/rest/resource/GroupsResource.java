2d3f0ca66de5a50e00467c5ac1d98ded40d0e346
Matt Fleming <7wjchnb836@snkmail.com>
2014-10-06 15:33:04 -0500

    Fixed up server side to do delete and save in a single transaction Fixed up client side to send the type down and show the link when the type is present

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/resource/GroupsResource.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/resource/GroupsResource.java
@@ -23,6 +23,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.annotation.security.RolesAllowed;
 import java.util.List;
+import java.util.Set;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
@@ -32,108 +33,95 @@ import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
  */
 
 @RestController
-@RequestMapping(value = "/webapi", produces = { APPLICATION_JSON_VALUE,
-		APPLICATION_XML_VALUE })
+@RequestMapping(value = "/webapi", produces = {APPLICATION_JSON_VALUE,
+        APPLICATION_XML_VALUE})
 public class GroupsResource {
 
-	@Resource
-	GroupService groupService;
+    @Resource
+    GroupService groupService;
 
-	@Resource
-	ReferenceGroupService referenceGroupService;
+    @Resource
+    ReferenceGroupService referenceGroupService;
 
-	@Resource
-	GroupResourceAssembler groupResourceAssembler;
-	
-	@Resource
-	ReferenceGroupResourceAssembler referenceGroupResourceAssembler;
-	
-	@Resource
-	TagService tagService;
-	
-	/**
-	 * GET to search for groups by client id
-	 *
-	 * @param pageable paged request
-	 * @param sort sorting request
-	 * @param status to filter by
-	 * @param assembler used to create the PagedResources
-	 * @param names to filter by
-	 * @return GroupPage or NO_CONTENT
-	 */
-	@RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.GET)
-	@RolesAllowed({ "PERM_GOD", "PERM_GROUP_LIST" })
+    @Resource
+    GroupResourceAssembler groupResourceAssembler;
+
+    @Resource
+    ReferenceGroupResourceAssembler referenceGroupResourceAssembler;
+
+    @Resource
+    TagService tagService;
+
+    /**
+     * GET to search for groups by client id
+     *
+     * @param pageable  paged request
+     * @param sort      sorting request
+     * @param assembler used to create the PagedResources
+     * @param clientId  to filter by
+     * @return GroupPage or NO_CONTENT
+     */
+    @RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.GET)
+    @RolesAllowed({"PERM_GOD", "PERM_GROUP_LIST"})
     @ApiImplicitParams(value = {
-            @ApiImplicitParam(name="size", defaultValue="50", value = "number of items on a page", dataType = "integer", paramType = "query"),
-            @ApiImplicitParam(name="page", defaultValue = "0", value = "page to request (zero index)", dataType = "integer", paramType = "query"),
-            @ApiImplicitParam(name="sort", defaultValue="id,asc", value = "sort to apply format: property,asc or desc", dataType = "string", paramType = "query")
+            @ApiImplicitParam(name = "size", defaultValue = "50", value = "number of items on a page", dataType = "integer", paramType = "query"),
+            @ApiImplicitParam(name = "page", defaultValue = "0", value = "page to request (zero index)", dataType = "integer", paramType = "query"),
+            @ApiImplicitParam(name = "sort", defaultValue = "id,asc", value = "sort to apply format: property,asc or desc", dataType = "string", paramType = "query")
     })
-	public ResponseEntity<GroupPage> listGroupsByClientID(
-			@PageableDefault(size = 50) Pageable pageable,
-			@SortDefault(sort = "id") Sort sort,
-			PagedResourcesAssembler<Group> assembler,
-			@PathVariable("clientId") Long clientId) {
+    public ResponseEntity<GroupPage> listGroupsByClientID(
+            @PageableDefault(size = 50) Pageable pageable,
+            @SortDefault(sort = "id") Sort sort,
+            PagedResourcesAssembler<Group> assembler,
+            @PathVariable("clientId") Long clientId) {
 
-		GroupSearchFilter groupSearchFilter = new GroupSearchFilter(clientId);
-		Page<Group> groupPage = groupService.list(pageable, groupSearchFilter);
+        GroupSearchFilter groupSearchFilter = new GroupSearchFilter(clientId);
+        Page<Group> groupPage = groupService.list(pageable, groupSearchFilter);
 
-		if (groupPage.hasContent()) {
-			return new ResponseEntity<>(new GroupPage(assembler.toResource(groupPage, groupResourceAssembler), groupPage, groupSearchFilter), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
-	}
-	
-	
-	/**
-	 * POST to save the groups and tags
-	 *
-	 * @param List<GroupSaveRequest>	groups and tags to create
-	 * @return ResponseEntity<List<Group>> or INTERNAL_SERVER_ERROR if update were
-	 *         unsuccessful
-	 */
-	@RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.POST)
-	@RolesAllowed({ "PERM_GOD", "PERM_GROUP_EDIT", "PERM_TAG_EDIT" })
-	public ResponseEntity<List<Group>> create(@RequestBody List<GroupSaveRequest> groupSaveRequests,
-			@PathVariable("clientId") Long clientId) {
+        if (groupPage.hasContent()) {
+            return new ResponseEntity<>(new GroupPage(assembler.toResource(groupPage, groupResourceAssembler), groupPage, groupSearchFilter), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
 
-		GroupSearchFilter groupSearchFilter = new GroupSearchFilter(clientId);
-		Page<Group> groupPage = groupService.list(groupSearchFilter);
-		
-		if (groupPage.hasContent() && !groupPage.getContent().isEmpty()) {
-			groupService.removeAll(groupPage.getContent());
-		}
-		if (!groupSaveRequests.isEmpty()) {
-			List<Group> groups = groupService.saveGroupsAndTags(groupSaveRequests, clientId);
 
-			if (groups == null || groups.isEmpty()) {
-				return new ResponseEntity<List<Group>>(HttpStatus.INTERNAL_SERVER_ERROR);
-			} else {
-				return new ResponseEntity<>(groups, HttpStatus.OK);
-			}
-		}
-		return null;
-	}
-	
-	
-	/**
-	 * GET To get group by id
-	 * 
-	 * @param id 	to load
-	 * @return GroupResource or NO_CONTENT on fail
-	 *
-	 */
-	@RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
-	@RolesAllowed({ "PERM_GOD", "PERM_GROUP_VIEW" })
-	public ResponseEntity<GroupResource> getGroupById(@PathVariable("id") Long id) {
-		Group group = groupService.reload(id);
-		if (group == null) {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		} else {
-			return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
-		}
-	}
-	
+    /**
+     * POST to save the groups and tags
+     *
+     * @param groupSaveRequests groups and tags to create
+     * @param clientId          the client id
+     * @return List of Group objects or INTERNAL_SERVER_ERROR if update were unsuccessful
+     */
+    @RequestMapping(value = "/clients/{clientId}/groups", method = RequestMethod.POST)
+    @RolesAllowed({"PERM_GOD", "PERM_GROUP_EDIT", "PERM_TAG_EDIT"})
+    public ResponseEntity<Set<Group>> create(@RequestBody List<GroupSaveRequest> groupSaveRequests,
+                                              @PathVariable("clientId") Long clientId) {
+        Set<Group> groups = groupService.saveGroupsAndTags(groupSaveRequests, clientId);
+        if (groups == null || groups.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        } else {
+            return new ResponseEntity<>(groups, HttpStatus.OK);
+        }
+    }
+
+
+    /**
+     * GET To get group by id
+     *
+     * @param id to load
+     * @return GroupResource or NO_CONTENT on fail
+     */
+    @RequestMapping(value = "/groups/{id}", method = RequestMethod.GET)
+    @RolesAllowed({"PERM_GOD", "PERM_GROUP_VIEW"})
+    public ResponseEntity<GroupResource> getGroupById(@PathVariable("id") Long id) {
+        Group group = groupService.reload(id);
+        if (group == null) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(groupResourceAssembler.toResource(group), HttpStatus.OK);
+        }
+    }
+
     /**
      * GET to retrieve ReferenceGroup data.
      *
@@ -147,14 +135,14 @@ public class GroupsResource {
     public ResponseEntity<ReferenceGroupPage> getRefGroups(@PageableDefault(size = 50) Pageable pageable,
                                                            @SortDefault(sort = "id") Sort sort,
                                                            PagedResourcesAssembler<ReferenceGroup> assembler) {
-    	
+
         Page<ReferenceGroup> groupPage = referenceGroupService.loadReferenceGroups(pageable);
-        
+
         if (groupPage.hasContent()) {
-			return new ResponseEntity<>(new ReferenceGroupPage(assembler.toResource(groupPage, referenceGroupResourceAssembler), groupPage), HttpStatus.OK);
-		} else {
-			return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-		}
+            return new ResponseEntity<>(new ReferenceGroupPage(assembler.toResource(groupPage, referenceGroupResourceAssembler), groupPage), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
     }
-    
+
 }
