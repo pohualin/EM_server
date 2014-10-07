72122255bd69e7c3f2e0890b51891de136d4e04f
Matt Fleming <7wjchnb836@snkmail.com>
2014-10-07 13:56:38 -0500

    EM-263 EM-217: Back-end. Now validates that group names cannot be the same (independent of special characters)

--- service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/GroupServiceImpl.java
+++ service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/GroupServiceImpl.java
@@ -5,6 +5,7 @@ import com.emmisolutions.emmimanager.persistence.GroupPersistence;
 import com.emmisolutions.emmimanager.service.ClientService;
 import com.emmisolutions.emmimanager.service.GroupService;
 import com.emmisolutions.emmimanager.service.TagService;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
@@ -50,9 +51,6 @@ public class GroupServiceImpl implements GroupService {
     @Override
     @Transactional(readOnly = true)
     public Group reload(Long id) {
-        if (id == null) {
-            return null;
-        }
         return groupPersistence.reload(id);
     }
 
@@ -60,28 +58,63 @@ public class GroupServiceImpl implements GroupService {
     @Override
     @Transactional
     public Set<Group> saveGroupsAndTags(List<GroupSaveRequest> groupSaveRequests, Long clientId) {
-        if (clientId == null) {
-            throw new IllegalArgumentException("clientID cannot be null");
-        }
-
-        // remove all groups for a client
-        groupPersistence.removeAll(clientId);
-
         Set<Group> groups = new HashSet<>();
         Client client = new Client();
         client.setId(clientId);
         client = clientService.reload(client);
 
-        for (GroupSaveRequest request : groupSaveRequests) {
-            request.getGroup().setClient(client);
-            Group savedGroup = save(request.getGroup());
-            List<Tag> savedTags = tagService.saveAllTagsForGroup(request.getTags(), savedGroup);
-            if (savedTags == null || savedTags.isEmpty()) {
-                throw new IllegalArgumentException("Tags cannot be null");
+        if (client != null) {
+            // make sure groups (and tags) are not duplicates
+            ensureGroupsAndTagsAreValid(groupSaveRequests);
+
+            // remove all groups for a client
+            groupPersistence.removeAll(clientId);
+
+            // add the groups back
+            for (GroupSaveRequest request : groupSaveRequests) {
+                if (request.getGroup() != null) {
+                    request.getGroup().setClient(client);
+                    Group savedGroup = save(request.getGroup());
+                    List<Tag> savedTags = tagService.saveAllTagsForGroup(request.getTags(), savedGroup);
+                    if (savedTags == null || savedTags.isEmpty()) {
+                        throw new IllegalArgumentException("Tags cannot be null");
+                    }
+                    savedGroup.setTags(new HashSet<>(savedTags));
+                    groups.add(savedGroup);
+                }
             }
-            savedGroup.setTags(new HashSet<>(savedTags));
-            groups.add(savedGroup);
         }
         return groups;
+    }
+
+    private void ensureGroupsAndTagsAreValid(List<GroupSaveRequest> saveRequests) {
+        Set<String> groupNames = new HashSet<>();
+        for (GroupSaveRequest saveRequest : saveRequests) {
+            Set<String> tagsInAGroup = new HashSet<>();
+            if (saveRequest.getGroup() != null) {
+                String normalizedName = normalizeName(saveRequest.getGroup().getName());
+                if (StringUtils.isBlank(normalizedName) || !groupNames.add(normalizedName)) {
+                    throw new IllegalArgumentException("Group name: '" + saveRequest.getGroup().getName() + "' only contains special characters or is a duplicate");
+                }
+                if (saveRequest.getTags() != null) {
+                    for (Tag tag : saveRequest.getTags()) {
+                        String normalizedTagName = normalizeName(tag.getName());
+                        if (StringUtils.isBlank(normalizedTagName) || !tagsInAGroup.add(normalizedTagName)) {
+                            // comment this in when EM-219/EM-310 is pulled into a sprint
+//                        throw new IllegalArgumentException("Tag name: '" + tag.getName() + "' only contains special characters or is a duplicate within group: " + saveRequest.getGroup().getName());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private String normalizeName(String name) {
+        String normalizedName = StringUtils.trimToEmpty(StringUtils.lowerCase(name));
+        if (StringUtils.isNotBlank(normalizedName)) {
+            // do regex
+            normalizedName = normalizedName.replaceAll("[^a-z0-9]*", "");
+        }
+        return normalizedName;
     }
 }
