fd7dc2fb79c84fa14e29a6b263c256042dc4aacb
Amrita Chavan <achavan@emmisolutions.com>
2014-10-16 10:15:59 -0500

    reverting any changes to TeamResourceAssembler

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/model/team/TeamResourceAssembler.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/model/team/TeamResourceAssembler.java
@@ -1,15 +1,15 @@
 package com.emmisolutions.emmimanager.web.rest.model.team;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
-
-import org.springframework.hateoas.ResourceAssembler;
-import org.springframework.stereotype.Component;
-
 import com.emmisolutions.emmimanager.model.Team;
 import com.emmisolutions.emmimanager.web.rest.model.provider.ProviderPage;
 import com.emmisolutions.emmimanager.web.rest.resource.TeamTagsResource;
 import com.emmisolutions.emmimanager.web.rest.resource.TeamsResource;
+
+import org.springframework.hateoas.ResourceAssembler;
+import org.springframework.stereotype.Component;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 /**
  * 
