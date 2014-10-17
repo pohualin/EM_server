2d9786401e706fbe837f6dec08ca00b471668b08
Desalvo, Claudio <Claudio.Desalvo@cmegroup.com>
2014-10-17 17:09:06 -0300

    EM-251

--- service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/TeamLocationServiceImpl.java
+++ service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/TeamLocationServiceImpl.java
@@ -1,5 +1,7 @@
 package com.emmisolutions.emmimanager.service.spring;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.annotation.Resource;
@@ -8,11 +10,13 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.emmisolutions.emmimanager.model.ClientLocationModificationRequest;
 import com.emmisolutions.emmimanager.model.Location;
 import com.emmisolutions.emmimanager.model.Team;
 import com.emmisolutions.emmimanager.model.TeamLocation;
 import com.emmisolutions.emmimanager.persistence.TeamLocationPersistence;
 import com.emmisolutions.emmimanager.persistence.TeamPersistence;
+import com.emmisolutions.emmimanager.service.LocationService;
 import com.emmisolutions.emmimanager.service.TeamLocationService;
 
 @Service
@@ -24,6 +28,9 @@ public class TeamLocationServiceImpl implements TeamLocationService {
     @Resource
     TeamPersistence teamPersistence;
 
+    @Resource
+    LocationService locationService;
+    
     @Override
     public Page<TeamLocation> findAllTeamLocationsWithTeam(Pageable pageable, Team team) {
         return teamLocationPersistence.getAllTeamLocationsForTeam(pageable, team);
@@ -32,12 +39,18 @@ public class TeamLocationServiceImpl implements TeamLocationService {
     @Override
     public void save(Team team, Set<Location> locationSet) {
         Team teamToFind = teamPersistence.reload(team);
+        List<Location> locationsToClient = new ArrayList<Location>();
         if(teamToFind != null && locationSet != null) {
             for (Location location : locationSet) {
                 TeamLocation teamLocation = new TeamLocation(location, teamToFind);
                 teamLocationPersistence.saveTeamLocation(teamLocation);
+                locationsToClient.add(location);
             }
         }
+        //after save the locations team, need to add those locations to the client
+        ClientLocationModificationRequest modificationRequest = new ClientLocationModificationRequest();
+        modificationRequest.setAdded(locationsToClient);
+        locationService.updateClientLocations(team.getClient(), modificationRequest);
     }
 
     @Override
