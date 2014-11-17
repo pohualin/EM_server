92aea949c2e6d0c42ea81e3ab0a78ca151ebc30f
Desalvo, Claudio <Claudio.Desalvo@cmegroup.com>
2014-11-17 17:48:40 -0300

    EM-490

--- service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/TeamLocationServiceImpl.java
+++ service-spring/src/main/java/com/emmisolutions/emmimanager/service/spring/TeamLocationServiceImpl.java
@@ -1,9 +1,23 @@
 package com.emmisolutions.emmimanager.service.spring;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.Resource;
+
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.emmisolutions.emmimanager.model.Client;
 import com.emmisolutions.emmimanager.model.Location;
 import com.emmisolutions.emmimanager.model.Team;
 import com.emmisolutions.emmimanager.model.TeamLocation;
+import com.emmisolutions.emmimanager.model.TeamProvider;
+import com.emmisolutions.emmimanager.model.TeamProviderTeamLocation;
 import com.emmisolutions.emmimanager.persistence.ClientLocationPersistence;
 import com.emmisolutions.emmimanager.persistence.TeamLocationPersistence;
 import com.emmisolutions.emmimanager.persistence.TeamPersistence;
@@ -11,14 +25,6 @@ import com.emmisolutions.emmimanager.persistence.TeamProviderTeamLocationPersist
 import com.emmisolutions.emmimanager.service.ClientService;
 import com.emmisolutions.emmimanager.service.LocationService;
 import com.emmisolutions.emmimanager.service.TeamLocationService;
-import org.springframework.dao.InvalidDataAccessApiUsageException;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
-import java.util.Set;
 
 @Service
 public class TeamLocationServiceImpl implements TeamLocationService {
@@ -48,12 +54,25 @@ public class TeamLocationServiceImpl implements TeamLocationService {
 
     @Override
     @Transactional
-    public void save(Team team, Set<Location> locationSet) {
+    public void save(Team team, Set<Location> locationSet, Set<TeamProvider> providerSet) {
         Team teamToFind = teamPersistence.reload(team);
         if(teamToFind != null && locationSet != null) {
             for (Location location : locationSet) {
                 TeamLocation teamLocation = new TeamLocation(location, teamToFind);
                 teamLocationPersistence.saveTeamLocation(teamLocation);
+                
+                List<TeamProviderTeamLocation> teamProviderTeamLocationsToSave = new ArrayList<TeamProviderTeamLocation>();
+            
+				for (TeamProvider teamProvider : providerSet) {
+					TeamProviderTeamLocation tptl = new TeamProviderTeamLocation();
+					tptl.setTeamProvider(teamProvider);
+					tptl.setTeamLocation(teamLocation);
+					teamProviderTeamLocationsToSave.add(tptl);
+				}
+                
+				if (teamProviderTeamLocationsToSave.size() > 0 ){
+					List<TeamProviderTeamLocation> savedTptls = teamProviderTeamLocationPersistence.saveAll(teamProviderTeamLocationsToSave);
+				}
                 // add to client location as well
                 clientLocationPersistence.create(location.getId(), teamToFind.getClient().getId());
             }
