ac87c767847f0afe22cdd68b37f17337058ad6ff
Matt Fleming <7wjchnb836@snkmail.com>
2014-11-24 16:47:33 -0600

    EM-458: team level roles

---
+++ persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/repo/UserClientTeamRoleRepository.java
@@ -0,0 +1,22 @@
+package com.emmisolutions.emmimanager.persistence.repo;
+
+import com.emmisolutions.emmimanager.model.user.client.UserClientTeamRole;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+/**
+ * Spring Data repo for ClientLocation objects
+ */
+public interface UserClientTeamRoleRepository extends JpaRepository<UserClientTeamRole, Long>, JpaSpecificationExecutor<UserClientTeamRole> {
+
+    /**
+     * Find all UserClientTeamRole objects for a client
+     *
+     * @param clientId to find for
+     * @param page     specification
+     * @return a page of UserClientTeamRole objects
+     */
+    Page<UserClientTeamRole> findByClientId(long clientId, Pageable page);
+}
---
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/model/user/client/team/UserClientTeamRoleResourcePage.java
@@ -0,0 +1,47 @@
+package com.emmisolutions.emmimanager.web.rest.model.user.client.team;
+
+import com.emmisolutions.emmimanager.model.Client;
+import com.emmisolutions.emmimanager.model.user.client.UserClientTeamRole;
+import com.emmisolutions.emmimanager.web.rest.model.PagedResource;
+import com.emmisolutions.emmimanager.web.rest.resource.ClientTeamRolesAdminResource;
+import org.springframework.data.domain.Page;
+import org.springframework.hateoas.*;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+
+/**
+ * A Page of UserClientRoleTeamResource objects
+ */
+public class UserClientTeamRoleResourcePage extends PagedResource<UserClientTeamRoleResource>{
+
+    public UserClientTeamRoleResourcePage() {
+    }
+
+    /**
+     * Wrapped constructor
+     *
+     * @param userClientTeamRoleResource to be wrapped
+     * @param userClientTeamRolePage      the raw response
+     */
+    public UserClientTeamRoleResourcePage(PagedResources<UserClientTeamRoleResource> userClientTeamRoleResource, Page<UserClientTeamRole> userClientTeamRolePage) {
+        pageDefaults(userClientTeamRoleResource, userClientTeamRolePage);
+    }
+
+    /**
+     * Create the search link
+     *
+     * @return Link for team roles on a client
+     * @see com.emmisolutions.emmimanager.web.rest.resource.ClientTeamRolesAdminResource#clientTeamRoles(Long, org.springframework.data.domain.Pageable, org.springframework.data.web.PagedResourcesAssembler)
+     */
+    public static Link createFullSearchLink(Client client) {
+        Link link = linkTo(methodOn(ClientTeamRolesAdminResource.class).clientTeamRoles(client.getId(), null, null)).withRel("teamRoles");
+        UriTemplate uriTemplate = new UriTemplate(link.getHref())
+            .with(new TemplateVariables(
+                new TemplateVariable("page", TemplateVariable.VariableType.REQUEST_PARAM),
+                new TemplateVariable("size", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED),
+                new TemplateVariable("sort", TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED)));
+        return new Link(uriTemplate, link.getRel());
+    }
+
+}
