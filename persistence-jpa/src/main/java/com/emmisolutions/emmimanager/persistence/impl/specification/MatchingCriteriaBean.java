cef8fadbcdfe9071588bd1a19e8b3118a56179cf
Matt Fleming <7wjchnb836@snkmail.com>
2014-12-17 21:13:17 -0600

    EM-491 A user admin can use an email as a user ID, or not

--- persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/specification/MatchingCriteriaBean.java
+++ persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/specification/MatchingCriteriaBean.java
@@ -5,9 +5,7 @@ import org.springframework.stereotype.Component;
 
 /**
  * This class handle all related to matching criteria in the search popups or
- * search text boxs
- *
- * @author claudiodesalvo
+ * search text boxes
  *
  */
 
@@ -21,56 +19,53 @@ public class MatchingCriteriaBean {
     /**
      * Remove all special characters to the name
      *
-     * @param name
-     *            to be normalized
+     * @param name to be normalized
      * @return normalized name to be used
      */
     public String normalizeName(String name) {
-	String normalizedName = StringUtils.trimToEmpty(StringUtils
-		.lowerCase(name));
-	if (StringUtils.isNotBlank(normalizedName)) {
-	    normalizedName = normalizedName.replaceAll("[^a-z0-9 ]*", "");
-	}
-	if (StringUtils.isBlank(normalizedName)) {
-	    normalizedName = name;
-	}
-	return normalizedName;
+        String normalizedName = StringUtils.trimToEmpty(StringUtils
+                .lowerCase(name));
+        if (StringUtils.isNotBlank(normalizedName)) {
+            normalizedName = normalizedName.replaceAll("[^a-z0-9 ]*", "");
+        }
+        if (StringUtils.isBlank(normalizedName)) {
+            normalizedName = name;
+        }
+        return normalizedName;
     }
 
     /**
      * Normalize a String... of names to be used to save in normalizedName
      * column
-     * 
-     * @param names
-     *            to be normalized
+     *
+     * @param names to be normalized
      * @return normalized name to be used
      */
     public String normalizedName(String... names) {
-	StringBuilder sb = new StringBuilder();
-	for (String name : names) {
-	    String normalized = StringUtils.trimToEmpty(StringUtils
-		    .lowerCase(name));
-	    if (StringUtils.isNotBlank(normalized)) {
-		normalized = normalized.replaceAll("[^a-z0-9]*", "");
-		sb.append(normalized);
-	    }
-	}
-	return sb.toString();
+        StringBuilder sb = new StringBuilder();
+        for (String name : names) {
+            String normalized = StringUtils.trimToEmpty(StringUtils
+                    .lowerCase(name));
+            if (StringUtils.isNotBlank(normalized)) {
+                normalized = normalized.replaceAll("[^a-z0-9]*", "");
+                sb.append(normalized);
+            }
+        }
+        return sb.toString();
     }
-    
+
     /**
      * Remove all special characters to the name including blank
      *
-     * @param name
-     *            to be normalized
+     * @param name to be normalized
      * @return normalized name to be used
      */
     public String normalizeNameAndBlank(String name) {
-    	String normalizedName = StringUtils.trimToEmpty(StringUtils.lowerCase(name));
-    	if (StringUtils.isNotBlank(normalizedName)){
-    	    // do regex
-    	    normalizedName = normalizedName.replaceAll("[^a-z0-9]*","");
-    	}
-    	return normalizedName;
+        String normalizedName = StringUtils.trimToEmpty(StringUtils.lowerCase(name));
+        if (StringUtils.isNotBlank(normalizedName)) {
+            // do regex
+            normalizedName = normalizedName.replaceAll("[^a-z0-9]*", "");
+        }
+        return normalizedName;
     }
 }
--- persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/specification/UserClientSpecifications.java
+++ persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/specification/UserClientSpecifications.java
@@ -1,22 +1,19 @@
 package com.emmisolutions.emmimanager.persistence.impl.specification;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.emmisolutions.emmimanager.model.UserClientSearchFilter;
+import com.emmisolutions.emmimanager.model.user.client.UserClient;
+import com.emmisolutions.emmimanager.model.user.client.UserClient_;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
-
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
-
-import com.emmisolutions.emmimanager.model.UserClientSearchFilter;
-import com.emmisolutions.emmimanager.model.user.client.UserClient;
-import com.emmisolutions.emmimanager.model.user.client.UserClient_;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * This is the specification class that allows for filtering of Client objects.
@@ -29,62 +26,97 @@ public class UserClientSpecifications {
 
     /**
      * Method to generate Specification with search term
-     * 
-     * @param filter
-     *            carriers search term
+     *
+     * @param filter carriers search term
      * @return Specification with search term
      */
     public Specification<UserClient> hasNames(
-	    final UserClientSearchFilter filter) {
-	return new Specification<UserClient>() {
-	    @Override
-	    public Predicate toPredicate(Root<UserClient> root,
-		    CriteriaQuery<?> query, CriteriaBuilder cb) {
-		if (filter != null && StringUtils.isNotBlank(filter.getTerm())) {
-		    List<String> searchTerms = new ArrayList<>();
-		    String[] terms = StringUtils.split(
-			    matchCriteria.normalizeName(filter.getTerm()), " ");
-		    if (terms != null) {
-			for (String term : terms) {
-			    if (!searchTerms.contains(term)) {
-				searchTerms.add(term);
-			    }
-			}
-		    }
-		    List<Predicate> andClause = new ArrayList<>();
-		    for (String searchTerm : searchTerms) {
-			andClause.add(cb.like(
-				root.get(UserClient_.normalizedName), "%"
-					+ searchTerm + "%"));
-		    }
-		    return cb.and(andClause.toArray(new Predicate[andClause
-			    .size()]));
-		}
-		return null;
-	    }
-	};
+            final UserClientSearchFilter filter) {
+        return new Specification<UserClient>() {
+            @Override
+            public Predicate toPredicate(Root<UserClient> root,
+                                         CriteriaQuery<?> query, CriteriaBuilder cb) {
+                if (filter != null && StringUtils.isNotBlank(filter.getTerm())) {
+                    List<String> searchTerms = new ArrayList<>();
+                    String[] terms = StringUtils.split(
+                            matchCriteria.normalizeName(filter.getTerm()), " ");
+                    if (terms != null) {
+                        for (String term : terms) {
+                            if (!searchTerms.contains(term)) {
+                                searchTerms.add(term);
+                            }
+                        }
+                    }
+                    List<Predicate> andClause = new ArrayList<>();
+                    for (String searchTerm : searchTerms) {
+                        andClause.add(cb.like(
+                                root.get(UserClient_.normalizedName), "%"
+                                        + searchTerm + "%"));
+                    }
+                    return cb.and(andClause.toArray(new Predicate[andClause
+                            .size()]));
+                }
+                return null;
+            }
+        };
     }
 
     /**
      * Method to generate Specification with clientId
-     * 
-     * @param filter
-     *            carries clientId
+     *
+     * @param filter carries clientId
      * @return Specification with clientID
      */
     public Specification<UserClient> isClient(
-	    final UserClientSearchFilter filter) {
-	return new Specification<UserClient>() {
-	    @Override
-	    public Predicate toPredicate(Root<UserClient> root,
-		    CriteriaQuery<?> query, CriteriaBuilder cb) {
-		if (filter != null && filter.getClientId() != null) {
-		    return cb.equal(root.get(UserClient_.client),
-			    filter.getClientId());
-		}
-		return null;
-	    }
-	};
+            final UserClientSearchFilter filter) {
+        return new Specification<UserClient>() {
+            @Override
+            public Predicate toPredicate(Root<UserClient> root,
+                                         CriteriaQuery<?> query, CriteriaBuilder cb) {
+                if (filter != null && filter.getClientId() != null) {
+                    return cb.equal(root.get(UserClient_.client),
+                            filter.getClientId());
+                }
+                return null;
+            }
+        };
     }
 
+    /**
+     * Find by login:
+     * e.g where UserClient.login == userClient.getLogin()
+     *
+     * @param userClient to filter by
+     * @return a specification or null
+     */
+    public Specification<UserClient> hasLogin(final UserClient userClient) {
+        return new Specification<UserClient>() {
+            @Override
+            public Predicate toPredicate(Root<UserClient> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                if (userClient != null && StringUtils.isNotBlank(userClient.getLogin())) {
+                    return cb.equal(root.get(UserClient_.login), userClient.getLogin().toLowerCase());
+                }
+                return null;
+            }
+        };
+    }
+
+    /**
+     * Find by email:
+     * e.g. where UserClient.email == userClient.getEmail()
+     *
+     * @param userClient to filter by
+     * @return a specification or null
+     */
+    public Specification<UserClient> hasEmail(final UserClient userClient) {
+        return new Specification<UserClient>() {
+            @Override
+            public Predicate toPredicate(Root<UserClient> root, CriteriaQuery<?> query, CriteriaBuilder cb) {
+                if (userClient != null && StringUtils.isNotBlank(userClient.getEmail())) {
+                    return cb.equal(root.get(UserClient_.email), userClient.getEmail().toLowerCase());
+                }
+                return null;
+            }
+        };
+    }
 }
