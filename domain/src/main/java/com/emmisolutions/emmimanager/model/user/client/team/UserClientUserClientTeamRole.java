c677fe1c483e88cf17583fff12465845ee0bda27
Paul Lin <plin@emmisolutions.com>
2014-12-22 10:56:37 -0600

    Update java format

--- domain/src/main/java/com/emmisolutions/emmimanager/model/user/client/team/UserClientUserClientTeamRole.java
+++ domain/src/main/java/com/emmisolutions/emmimanager/model/user/client/team/UserClientUserClientTeamRole.java
@@ -14,26 +14,29 @@ import javax.validation.constraints.NotNull;
  */
 @Audited
 @Entity
-@Table(name = "user_client_user_client_team_role", uniqueConstraints = @UniqueConstraint(columnNames = {
-	"users_id", "client_team_id", "user_client_team_role_id" }, name = "uk_user_client_user_client_team_role"))
+@Table(name = "user_client_user_client_team_role",
+    uniqueConstraints =
+    @UniqueConstraint(columnNames = {"users_id", "client_team_id","user_client_team_role_id"},
+        name = "uk_user_client_user_client_team_role")
+)
 public class UserClientUserClientTeamRole extends AbstractAuditingEntity {
 
-    /**
-     * Default constructor
-     */
-    public UserClientUserClientTeamRole() {
+	/**
+	 * Default constructor
+	 */
+	public UserClientUserClientTeamRole() {
 
-    }
+	}
 
-    /**
-     * Constructor with id
-     * 
-     * @param id
-     */
-    public UserClientUserClientTeamRole(Long id) {
-	this.id = id;
-    }
-
+	/**
+	 * Constructor with id
+	 * 
+	 * @param id
+	 */
+	public UserClientUserClientTeamRole(Long id) {
+		this.id = id;
+	}
+	
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "bigint")
@@ -41,69 +44,76 @@ public class UserClientUserClientTeamRole extends AbstractAuditingEntity {
 
     @NotNull
     @ManyToOne(optional = false)
-    @JoinColumn(name = "user_client_team_role_id", nullable = false, columnDefinition = "bigint", foreignKey = @ForeignKey(name = "fk_user_client_user_client_team_role_role"))
+    @JoinColumn(name = "user_client_team_role_id", nullable = false, columnDefinition = "bigint",
+        foreignKey = @ForeignKey(name = "fk_user_client_user_client_team_role_role")
+    )
     private UserClientTeamRole userClientTeamRole;
 
     @ManyToOne(optional = false)
-    @JoinColumn(name = "client_team_id", nullable = false, columnDefinition = "bigint", foreignKey = @ForeignKey(name = "fk_user_client_client_team"))
+    @JoinColumn(name = "client_team_id", nullable = false, columnDefinition = "bigint",
+        foreignKey = @ForeignKey(name = "fk_user_client_client_team")
+    )
     private Team team;
 
     @NotNull
     @ManyToOne(optional = false)
-    @JoinColumn(name = "users_id", nullable = false, columnDefinition = "bigint", foreignKey = @ForeignKey(name = "fk_user_client_user_client_team_role_users"))
+    @JoinColumn(name = "users_id", nullable = false, columnDefinition = "bigint",
+        foreignKey = @ForeignKey(name = "fk_user_client_user_client_team_role_users")
+    )
     private UserClient userClient;
 
     public Long getId() {
-	return id;
+        return id;
     }
 
     public void setId(Long id) {
-	this.id = id;
+        this.id = id;
     }
 
     public UserClientTeamRole getUserClientTeamRole() {
-	return userClientTeamRole;
+        return userClientTeamRole;
     }
 
     public void setUserClientTeamRole(UserClientTeamRole userClientTeamRole) {
-	this.userClientTeamRole = userClientTeamRole;
+        this.userClientTeamRole = userClientTeamRole;
     }
 
     public UserClient getUserClient() {
-	return userClient;
+        return userClient;
     }
 
     public void setUserClient(UserClient userClient) {
-	this.userClient = userClient;
+        this.userClient = userClient;
     }
 
     public Team getTeam() {
-	return team;
+        return team;
     }
 
     public void setTeam(Team team) {
-	this.team = team;
+        this.team = team;
     }
 
     @Override
     public boolean equals(Object o) {
-	if (this == o)
-	    return true;
-	if (o == null || getClass() != o.getClass())
-	    return false;
-	UserClientUserClientTeamRole that = (UserClientUserClientTeamRole) o;
-	return !(id != null ? !id.equals(that.id) : that.id != null);
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserClientUserClientTeamRole that = (UserClientUserClientTeamRole) o;
+        return !(id != null ? !id.equals(that.id) : that.id != null);
     }
 
     @Override
     public int hashCode() {
-	return id != null ? id.hashCode() : 0;
+        return id != null ? id.hashCode() : 0;
     }
 
     @Override
     public String toString() {
-	return "UserClientUserClientTeamRole{" + "id=" + id
-		+ ", userClientTeamRole=" + userClientTeamRole + ", team="
-		+ team + ", userClient=" + userClient + '}';
+        return "UserClientUserClientTeamRole{" +
+            "id=" + id +
+            ", userClientTeamRole=" + userClientTeamRole +
+            ", team=" + team +
+            ", userClient=" + userClient +
+            '}';
     }
 }
--- persistence-jpa/src/test/java/com/emmisolutions/emmimanager/persistence/BaseIntegrationTest.java
+++ persistence-jpa/src/test/java/com/emmisolutions/emmimanager/persistence/BaseIntegrationTest.java
@@ -1,6 +1,11 @@
 package com.emmisolutions.emmimanager.persistence;
 
-import javax.annotation.Resource;
+import com.emmisolutions.emmimanager.model.*;
+import com.emmisolutions.emmimanager.model.user.admin.UserAdmin;
+import com.emmisolutions.emmimanager.model.user.client.UserClient;
+import com.emmisolutions.emmimanager.model.user.client.UserClientRole;
+import com.emmisolutions.emmimanager.model.user.client.team.UserClientTeamRole;
+import com.emmisolutions.emmimanager.persistence.configuration.PersistenceConfiguration;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.joda.time.LocalDate;
@@ -13,21 +18,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.emmisolutions.emmimanager.model.Client;
-import com.emmisolutions.emmimanager.model.ClientRegion;
-import com.emmisolutions.emmimanager.model.ClientTier;
-import com.emmisolutions.emmimanager.model.ClientType;
-import com.emmisolutions.emmimanager.model.Location;
-import com.emmisolutions.emmimanager.model.Provider;
-import com.emmisolutions.emmimanager.model.SalesForce;
-import com.emmisolutions.emmimanager.model.State;
-import com.emmisolutions.emmimanager.model.Team;
-import com.emmisolutions.emmimanager.model.TeamSalesForce;
-import com.emmisolutions.emmimanager.model.user.admin.UserAdmin;
-import com.emmisolutions.emmimanager.model.user.client.UserClient;
-import com.emmisolutions.emmimanager.model.user.client.UserClientRole;
-import com.emmisolutions.emmimanager.model.user.client.team.UserClientTeamRole;
-import com.emmisolutions.emmimanager.persistence.configuration.PersistenceConfiguration;
+import javax.annotation.Resource;
 
 /**
  * Root integration test harness
@@ -39,167 +30,160 @@ import com.emmisolutions.emmimanager.persistence.configuration.PersistenceConfig
 @Transactional
 public abstract class BaseIntegrationTest {
 
-	@Resource
-	UserPersistence userPersistence;
+    @Resource
+    UserPersistence userPersistence;
 
-	@Resource
-	ClientPersistence clientPersistence;
+    @Resource
+    ClientPersistence clientPersistence;
 
-	@Resource
-	TeamPersistence teamPersistence;
+    @Resource
+    TeamPersistence teamPersistence;
 
-	@Resource
-	ProviderPersistence providerPersistence;
+    @Resource
+    ProviderPersistence providerPersistence;
 
-	@Resource
-	LocationPersistence locationPersistence;
+    @Resource
+    LocationPersistence locationPersistence;
 
-	@Resource
-	UserClientRolePersistence userClientRolePersistence;
+    @Resource
+    UserClientRolePersistence userClientRolePersistence;
+    
+    @Resource
+    UserClientTeamRolePersistence userClientTeamRolePersistence;
 
-	@Resource
-	UserClientTeamRolePersistence userClientTeamRolePersistence;
+    @Resource
+    UserClientPersistence userClientPersistence;
 
-	@Resource
-	UserClientPersistence userClientPersistence;
+    /**
+     * Login as a user
+     *
+     * @param login to login as
+     */
+    protected void login(String login) {
+        SecurityContextHolder.getContext().setAuthentication(new UsernamePasswordAuthenticationToken(login, "******"));
+    }
 
-	/**
-	 * Login as a user
-	 * 
-	 * @param login
-	 *            to login as
-	 */
-	protected void login(String login) {
-		SecurityContextHolder.getContext().setAuthentication(
-				new UsernamePasswordAuthenticationToken(login, "******"));
-	}
+    /**
+     * Logout of the system..
+     */
+    protected void logout() {
+        SecurityContextHolder.clearContext();
+    }
 
-	/**
-	 * Logout of the system..
-	 */
-	protected void logout() {
-		SecurityContextHolder.clearContext();
-		SecurityContextHolder.clearContext();
-	}
+    /**
+     * Creates a brand new client that shouldn't already be inserted
+     *
+     * @return random client
+     */
+    protected Client makeNewRandomClient() {
+        Client client = new Client();
+        client.setTier(new ClientTier(3l));
+        client.setContractEnd(LocalDate.now().plusYears(1));
+        client.setContractStart(LocalDate.now());
+        client.setRegion(new ClientRegion(1l));
+        client.setName(RandomStringUtils.randomAlphanumeric(255));
+        client.setType(new ClientType(1l));
+        client.setActive(true);
+        client.setContractOwner(makeNewRandomUserAdmin());
+        client.setSalesForceAccount(new SalesForce(RandomStringUtils.randomAlphanumeric(18)));
+        return clientPersistence.save(client);
+    }
 
-	/**
-	 * Creates a brand new client that shouldn't already be inserted
-	 *
-	 * @return random client
-	 */
-	protected Client makeNewRandomClient() {
-		Client client = new Client();
-		client.setTier(new ClientTier(3l));
-		client.setContractEnd(LocalDate.now().plusYears(1));
-		client.setContractStart(LocalDate.now());
-		client.setRegion(new ClientRegion(1l));
-		client.setName(RandomStringUtils.randomAlphanumeric(255));
-		client.setType(new ClientType(1l));
-		client.setActive(true);
-		client.setContractOwner(makeNewRandomUserAdmin());
-		client.setSalesForceAccount(new SalesForce(RandomStringUtils
-				.randomAlphanumeric(18)));
-		return clientPersistence.save(client);
-	}
+    /**
+     * Makes a new randomized team
+     *
+     * @return random team
+     */
+    protected Team makeNewRandomTeam() {
+        Team team = new Team();
+        team.setName(RandomStringUtils.randomAlphanumeric(100));
+        team.setClient(makeNewRandomClient());
+        team.setActive(true);
+        team.setSalesForceAccount(new TeamSalesForce(RandomStringUtils.randomAlphanumeric(18)));
+        team.setDescription(RandomStringUtils.randomAlphabetic(255));
+        return teamPersistence.save(team);
+    }
 
-	/**
-	 * Makes a new randomized team
-	 *
-	 * @return random team
-	 */
-	protected Team makeNewRandomTeam() {
-		Team team = new Team();
-		team.setName(RandomStringUtils.randomAlphanumeric(100));
-		team.setClient(makeNewRandomClient());
-		team.setActive(true);
-		team.setSalesForceAccount(new TeamSalesForce(RandomStringUtils
-				.randomAlphanumeric(18)));
-		team.setDescription(RandomStringUtils.randomAlphabetic(255));
-		return teamPersistence.save(team);
-	}
+    /**
+     * Make a new randomized Provider
+     *
+     * @return random provider
+     */
+    protected Provider makeNewRandomProvider() {
+        Provider provider = new Provider();
+        provider.setFirstName(RandomStringUtils.randomAlphabetic(255));
+        provider.setLastName(RandomStringUtils.randomAlphabetic(255));
+        provider.setActive(true);
+        return providerPersistence.save(provider);
+    }
 
-	/**
-	 * Make a new randomized Provider
-	 * 
-	 * @return random provider
-	 */
-	protected Provider makeNewRandomProvider() {
-		Provider provider = new Provider();
-		provider.setFirstName(RandomStringUtils.randomAlphabetic(255));
-		provider.setLastName(RandomStringUtils.randomAlphabetic(255));
-		provider.setActive(true);
-		return providerPersistence.save(provider);
-	}
+    /**
+     * Create a new randomized location
+     *
+     * @return the saved location
+     */
+    protected Location makeNewRandomLocation() {
+        Location location = new Location();
+        location.setActive(true);
+        location.setName(RandomStringUtils.randomAlphanumeric(255));
+        location.setCity(RandomStringUtils.randomAlphanumeric(255));
+        location.setPhone("214-555-5555");
+        location.setState(State.TX);
+        return locationPersistence.save(location);
+    }
 
-	/**
-	 * Create a new randomized location
-	 * 
-	 * @return the saved location
-	 */
-	protected Location makeNewRandomLocation() {
-		Location location = new Location();
-		location.setActive(true);
-		location.setName(RandomStringUtils.randomAlphanumeric(255));
-		location.setCity(RandomStringUtils.randomAlphanumeric(255));
-		location.setPhone("214-555-5555");
-		location.setState(State.TX);
-		return locationPersistence.save(location);
-	}
+    /**
+     * Creates a new UserClient
+     *
+     * @return a UserClient
+     */
+    protected UserClient makeNewRandomUserClient(Client client) {
+        UserClient userClient = new UserClient();
+        userClient.setClient(client != null ? client : makeNewRandomClient());
+        userClient.setFirstName("a" + RandomStringUtils.randomAlphabetic(49));
+        userClient.setLastName(RandomStringUtils.randomAlphabetic(50));
+        userClient.setLogin(RandomStringUtils.randomAlphabetic(255));
+        return userClientPersistence.saveOrUpdate(userClient);
+    }
 
-	/**
-	 * Creates a new UserClient
-	 *
-	 * @return a UserClient
-	 */
-	protected UserClient makeNewRandomUserClient(Client client) {
-		UserClient userClient = new UserClient();
-		userClient.setClient(client != null ? client : makeNewRandomClient());
-		userClient.setFirstName("a" + RandomStringUtils.randomAlphabetic(49));
-		userClient.setLastName(RandomStringUtils.randomAlphabetic(50));
-		userClient.setLogin(RandomStringUtils.randomAlphabetic(255));
-		return userClientPersistence.saveOrUpdate(userClient);
-	}
+    /**
+     * Creates a new UserClientRole
+     *
+     * @return a UserClientRole
+     */
+    protected UserClientRole makeNewRandomUserClientRole(Client client) {
+        if (client == null) {
+            client = makeNewRandomClient();
+        }
+        UserClientRole userClientRole = new UserClientRole(RandomStringUtils.randomAlphabetic(10), client, null);
+        return userClientRolePersistence.save(userClientRole);
+    }
+    
+    /**
+     * Create a random UserClientTeamRole
+     */
+    protected UserClientTeamRole makeNewRandomUserClientTeamRole(Client client){
+    	 if (client == null) {
+             client = makeNewRandomClient();
+         }
+         UserClientTeamRole userClientTeamRole = new UserClientTeamRole();
+         userClientTeamRole.setName(RandomStringUtils.randomAlphabetic(50));
+         userClientTeamRole.setClient(client);
+         return userClientTeamRolePersistence.save(userClientTeamRole);
+    }
 
-	/**
-	 * Creates a new UserClientRole
-	 *
-	 * @return a UserClientRole
-	 */
-	protected UserClientRole makeNewRandomUserClientRole(Client client) {
-		if (client == null) {
-			client = makeNewRandomClient();
-		}
-		UserClientRole userClientRole = new UserClientRole(
-				RandomStringUtils.randomAlphabetic(10), client, null);
-		return userClientRolePersistence.save(userClientRole);
-	}
-
-	/**
-	 * Creates a new UserClientTeamRole
-	 *
-	 * @return a UserClientRole
-	 */
-	protected UserClientTeamRole makeNewRandomUserClientTeamRole(Client client) {
-		if (client == null) {
-			client = makeNewRandomClient();
-		}
-		UserClientTeamRole userClientTeamRole = new UserClientTeamRole(
-				RandomStringUtils.randomAlphabetic(10), client, null);
-		return userClientTeamRolePersistence.save(userClientTeamRole);
-	}
-
-	/**
-	 * Creates a new UserAdmin
-	 *
-	 * @return a UserAdmin
-	 */
-	protected UserAdmin makeNewRandomUserAdmin() {
-		UserAdmin userAdmin = new UserAdmin(
-				RandomStringUtils.randomAlphabetic(255),
-				RandomStringUtils.randomAlphanumeric(100));
-		userAdmin.setFirstName(RandomStringUtils.randomAlphabetic(50));
-		userAdmin.setLastName(RandomStringUtils.randomAlphabetic(50));
-		return userPersistence.saveOrUpdate(userAdmin);
-	}
+    /**
+     * Creates a new UserAdmin
+     *
+     * @return a UserAdmin
+     */
+    protected UserAdmin makeNewRandomUserAdmin() {
+        UserAdmin userAdmin = new UserAdmin(RandomStringUtils
+                .randomAlphabetic(255), RandomStringUtils
+                .randomAlphanumeric(100));
+        userAdmin.setFirstName(RandomStringUtils.randomAlphabetic(50));
+        userAdmin.setLastName(RandomStringUtils.randomAlphabetic(50));
+        return userPersistence.saveOrUpdate(userAdmin);
+    }
 
 }
