0a8d9881d2eb87ea2d1cb0f7ee8bb20c1b8f94ab
Amrita Chavan <achavan@emmisolutions.com>
2014-12-22 18:11:50 -0600

    tests

--- service-spring/src/test/java/com/emmisolutions/emmimanager/service/spring/TeamProviderTeamLocationServiceIntegrationTest.java
+++ service-spring/src/test/java/com/emmisolutions/emmimanager/service/spring/TeamProviderTeamLocationServiceIntegrationTest.java
@@ -1,5 +1,9 @@
 package com.emmisolutions.emmimanager.service.spring;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -101,7 +105,8 @@ public class TeamProviderTeamLocationServiceIntegrationTest extends
 		teamProvider.setProvider(provider);
 		teamProviderPersistence.save(teamProvider);
 
-		teamProviderTeamLocationService.createTeamProviderTeamLocation(teamLocations, teamProvider);
+		Set<TeamProviderTeamLocation> savedTPTLs = teamProviderTeamLocationService.createTeamProviderTeamLocation(teamLocations, teamProvider);
+        assertThat("TPTL has been created", savedTPTLs.size(), is(2));
 	}
 
 	@Test
