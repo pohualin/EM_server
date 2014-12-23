3720fbf2f894b52ff46ac8d44989afc8f79647eb
Paul Lin <plin@emmisolutions.com>
2014-12-23 12:20:18 -0600

    Test new formatter

--- domain/src/main/java/com/emmisolutions/emmimanager/model/UserClientUserClientTeamRoleSearchFilter.java
+++ domain/src/main/java/com/emmisolutions/emmimanager/model/UserClientUserClientTeamRoleSearchFilter.java
@@ -13,105 +13,105 @@ import com.emmisolutions.emmimanager.model.user.client.UserClient;
 @XmlAccessorType(XmlAccessType.FIELD)
 public class UserClientUserClientTeamRoleSearchFilter {
 
-	private String term;
+    private String term;
 
-	private StatusFilter status;
+    private StatusFilter status;
 
-	private UserClient userClient;
+    private UserClient userClient;
 
-	/**
-	 * Default constructor
-	 */
-	public UserClientUserClientTeamRoleSearchFilter() {
-		this.status = StatusFilter.ALL;
-	}
+    /**
+     * Default constructor
+     */
+    public UserClientUserClientTeamRoleSearchFilter() {
+        this.status = StatusFilter.ALL;
+    }
 
-	/**
-	 * Constructor with passed in userClient and search term
-	 * 
-	 * @param userClient
-	 *            to use
-	 * @param term
-	 *            to search
-	 */
-	public UserClientUserClientTeamRoleSearchFilter(UserClient userClient,
-			String term) {
-		this(userClient, StatusFilter.ALL, term);
-	}
+    /**
+     * Constructor with passed in userClient and search term
+     * 
+     * @param userClient
+     *            to use
+     * @param term
+     *            to search
+     */
+    public UserClientUserClientTeamRoleSearchFilter(UserClient userClient,
+            String term) {
+        this(userClient, StatusFilter.ALL, term);
+    }
 
-	/**
-	 * Constructor with passed in userClient, status and search term
-	 * 
-	 * @param userClient
-	 *            to use
-	 * @param status
-	 *            to filter
-	 * @param term
-	 *            to search
-	 */
-	public UserClientUserClientTeamRoleSearchFilter(UserClient userClient,
-			StatusFilter status, String term) {
-		this.userClient = userClient;
-		this.term = term;
-		this.status = status;
-	}
+    /**
+     * Constructor with passed in userClient, status and search term
+     * 
+     * @param userClient
+     *            to use
+     * @param status
+     *            to filter
+     * @param term
+     *            to search
+     */
+    public UserClientUserClientTeamRoleSearchFilter(UserClient userClient,
+            StatusFilter status, String term) {
+        this.userClient = userClient;
+        this.term = term;
+        this.status = status;
+    }
 
-	/**
-	 * Return term
-	 * 
-	 * @return term
-	 */
-	public String getTerm() {
-		return term;
-	}
+    /**
+     * Return term
+     * 
+     * @return term
+     */
+    public String getTerm() {
+        return term;
+    }
 
-	/**
-	 * Return status
-	 * 
-	 * @return status
-	 */
-	public StatusFilter getStatus() {
-		return status;
-	}
+    /**
+     * Return status
+     * 
+     * @return status
+     */
+    public StatusFilter getStatus() {
+        return status;
+    }
 
-	/**
-	 * Return userClient
-	 * 
-	 * @return userClient
-	 */
-	public UserClient getUserClient() {
-		return userClient;
-	}
+    /**
+     * Return userClient
+     * 
+     * @return userClient
+     */
+    public UserClient getUserClient() {
+        return userClient;
+    }
 
-	/**
-	 * Status allowed
-	 */
-	public enum StatusFilter {
-		ALL, ACTIVE_ONLY, INACTIVE_ONLY;
+    /**
+     * Status allowed
+     */
+    public enum StatusFilter {
+        ALL, ACTIVE_ONLY, INACTIVE_ONLY;
 
-		/**
-		 * from string or ACTIVE
-		 * 
-		 * @param status
-		 *            the status string
-		 * @return never null, the status or ACTIVE
-		 */
-		public static StatusFilter fromStringOrActive(String status) {
-			if (StringUtils.isNotBlank(status)) {
-				for (StatusFilter statusFilter : values()) {
-					status = status.toUpperCase();
-					if (statusFilter.toString().equals(status)) {
-						return statusFilter;
-					}
-				}
-			}
-			return ACTIVE_ONLY;
-		}
-	}
+        /**
+         * from string or ACTIVE
+         * 
+         * @param status
+         *            the status string
+         * @return never null, the status or ACTIVE
+         */
+        public static StatusFilter fromStringOrActive(String status) {
+            if (StringUtils.isNotBlank(status)) {
+                for (StatusFilter statusFilter : values()) {
+                    status = status.toUpperCase();
+                    if (statusFilter.toString().equals(status)) {
+                        return statusFilter;
+                    }
+                }
+            }
+            return ACTIVE_ONLY;
+        }
+    }
 
-	@Override
-	public String toString() {
-		return "UserClientSearchFilter{" + "term=" + term + ", status="
-				+ status + '}';
-	}
+    @Override
+    public String toString() {
+        return "UserClientSearchFilter{" + "term=" + term + ", status="
+                + status + '}';
+    }
 }
