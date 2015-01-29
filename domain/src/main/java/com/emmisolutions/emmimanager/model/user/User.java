60ee32525c87b9581959f79f0c6678411c612570
Matt Fleming <7wjchnb836@snkmail.com>
2015-01-29 11:34:57 -0600

    Switched the users to a JOINED inheritance strategy

--- domain/src/main/java/com/emmisolutions/emmimanager/model/user/User.java
+++ domain/src/main/java/com/emmisolutions/emmimanager/model/user/User.java
@@ -1,9 +1,9 @@
 package com.emmisolutions.emmimanager.model.user;
 
 import com.emmisolutions.emmimanager.model.AbstractAuditingEntity;
+import org.apache.commons.lang3.StringUtils;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.NotAudited;
-import org.hibernate.validator.constraints.Email;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -12,7 +12,6 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
 import java.io.Serializable;
 import java.util.Collection;
 
@@ -21,43 +20,21 @@ import java.util.Collection;
  */
 @Entity
 @Audited
-@Table(name = "users",
-        uniqueConstraints = {
-                @UniqueConstraint(columnNames = {"login"}, name = "uk_users_login")
-        },
-        indexes = {
-                @Index(name = "ix_users_email", columnList = "email", unique = true)
-        }
-)
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-@DiscriminatorColumn(
-        name = "user_type", discriminatorType = DiscriminatorType.STRING)
+@Table(name = "users")
+@Inheritance(strategy = InheritanceType.JOINED)
+@DiscriminatorColumn(name = "user_type",
+        discriminatorType = DiscriminatorType.STRING)
 @XmlRootElement(name = "user")
-public class User extends AbstractAuditingEntity implements Serializable, UserDetails {
+public abstract class User extends AbstractAuditingEntity implements Serializable, UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "bigint")
     private Long id;
 
-    @NotNull
-    @Size(min = 0, max = 255)
-    @Column(length = 255, nullable = false, columnDefinition = "nvarchar(255)")
-    private String login;
-
     @Version
     @Column(columnDefinition = "int")
     private Integer version;
-
-    @Column(length = 40, columnDefinition = "varchar(40)")
-    @Size(min = 0, max = 40)
-    @XmlTransient
-    private String password;
-
-    @Column(length = 32, columnDefinition = "varchar(32)")
-    @Size(min = 0, max = 32)
-    @XmlTransient
-    private String salt;
 
     @NotNull
     @Size(min = 0, max = 50)
@@ -80,43 +57,12 @@ public class User extends AbstractAuditingEntity implements Serializable, UserDe
     @Column(name = "last_name", length = 50, nullable = false, columnDefinition = "nvarchar(50)")
     private String lastName;
 
-    @Email
-    @Size(min = 0, max = 255)
-    @Column(length = 255, columnDefinition = "nvarchar(255)")
-    private String email;
-
     @NotNull
     @Size(max = 610)
     @Column(name = "normalized_name", length = 610, nullable = false, columnDefinition = "nvarchar(610)")
     @NotAudited
     @Pattern(regexp = "[a-z0-9]*", message = "Normalized name can only contain lowercase letters and digits")
     private String normalizedName;
-
-    public User() {
-
-    }
-
-    /**
-     * Constructor for security based things
-     *
-     * @param login    the login
-     * @param password the password
-     */
-    public User(String login, String password) {
-        this.login = login;
-        this.password = password;
-    }
-
-    /**
-     * Constructor with ID only
-     *
-     * @param id      the id
-     * @param version the version
-     */
-    public User(Long id, Integer version) {
-        this.id = id;
-        this.version = version;
-    }
 
     public Long getId() {
         return id;
@@ -134,19 +80,10 @@ public class User extends AbstractAuditingEntity implements Serializable, UserDe
         this.version = version;
     }
 
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
     @Override
     public String toString() {
         return getClass().getSimpleName() + "{" +
                 "id=" + id +
-                ", login='" + login + '\'' +
                 ", version=" + version +
                 ", active=" + active +
                 '}';
@@ -170,21 +107,18 @@ public class User extends AbstractAuditingEntity implements Serializable, UserDe
         return null;
     }
 
-    public String getPassword() {
-        return password;
-    }
-
-    @Override
-    public String getUsername() {
-        return login;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public String getFirstName() {
         return firstName;
+    }
+
+    /**
+     * Creates the full name string (first + last)
+     *
+     * @return the full name or blank
+     */
+    public String getFullName() {
+        return StringUtils.trimToEmpty(StringUtils.defaultString(getFirstName()) +
+                " " + StringUtils.defaultString(getLastName()));
     }
 
     public void setFirstName(String firstName) {
@@ -197,14 +131,6 @@ public class User extends AbstractAuditingEntity implements Serializable, UserDe
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getNormalizedName() {
@@ -221,14 +147,6 @@ public class User extends AbstractAuditingEntity implements Serializable, UserDe
 
     public void setActive(boolean active) {
         this.active = active;
-    }
-
-    public String getSalt() {
-        return salt;
-    }
-
-    public void setSalt(String salt) {
-        this.salt = salt;
     }
 
     @Override
