03d2d8125fab77e2f636e1c46e56df2b6161a82b
Amrita Chavan <achavan@emmisolutions.com>
2015-03-10 13:58:34 -0500

    EM-767: fixing dependency

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/resource/UserClientsResource.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/resource/UserClientsResource.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.emmisolutions.emmimanager.model.user.User;
 import com.emmisolutions.emmimanager.model.user.client.UserClient;
 import com.emmisolutions.emmimanager.service.UserClientService;
 import com.emmisolutions.emmimanager.service.UserClientService.UserClientConflict;
@@ -41,8 +40,8 @@ public class UserClientsResource {
     UserDetailsService userDetailsService;
 
     @Resource(name = "userClientAuthenticationResourceAssembler")
-    ResourceAssembler<User, UserClientResource> userResourceAssembler;
-
+    ResourceAssembler<UserClient, UserClientResource> userResourceAssembler;
+    
     @Resource
     UserClientResourceAssembler userClientResourceAssembler;
 
