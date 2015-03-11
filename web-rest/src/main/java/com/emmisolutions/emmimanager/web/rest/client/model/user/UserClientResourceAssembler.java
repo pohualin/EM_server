b4bd89fc80a8bfc8adcec52bbfa201da0ff0cef7
Amrita Chavan <achavan@emmisolutions.com>
2015-03-11 13:48:40 -0500

    Merge branch 'em-797'
    Conflicts:
    	web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/model/user/UserClientResourceAssembler.java

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/model/user/UserClientResourceAssembler.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/model/user/UserClientResourceAssembler.java
@@ -4,6 +4,7 @@ import com.emmisolutions.emmimanager.model.Client;
 import com.emmisolutions.emmimanager.model.user.client.UserClient;
 import com.emmisolutions.emmimanager.web.rest.admin.model.client.ClientResource;
 import com.emmisolutions.emmimanager.web.rest.client.resource.UserClientSecretQuestionResponsesResource;
+import com.emmisolutions.emmimanager.web.rest.client.resource.UserClientsPasswordResource;
 import com.emmisolutions.emmimanager.web.rest.client.resource.UserClientsResource;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.security.core.GrantedAuthority;
@@ -44,7 +45,8 @@ public class UserClientResourceAssembler implements ResourceAssembler<UserClient
             }
 
         }
-        ClientResource clientResource = user instanceof UserClient ? clientResourceAssembler.toResource(((UserClient) user).getClient()) : null;
+        ClientResource clientResource =
+                clientResourceAssembler.toResource(((UserClient) user).getClient());
 
         UserClientResource ret = new UserClientResource(
                 user.getId(),
@@ -60,12 +62,14 @@ public class UserClientResourceAssembler implements ResourceAssembler<UserClient
                 user.isEmailValidated(),
                 clientResource,
                 perms,
-                user.isImpersonated(), user.getPasswordExpireationDateTime());
+                user.isImpersonated(),
+                user.getPasswordExpireationDateTime());
         ret.add(linkTo(methodOn(UserClientsResource.class).getById(user.getId())).withSelfRel());
         ret.add(linkTo(methodOn(UserClientsResource.class).authenticated()).withRel("authenticated"));
         if (!user.isImpersonated()) {
             ret.add(linkTo(methodOn(UserClientSecretQuestionResponsesResource.class).secretQuestionResponses(user.getId(), null, null)).withRel("secretQuestionResponses"));
             ret.add(linkTo(methodOn(UserClientsResource.class).validate(user.getId(), null)).withRel("validate"));
+            ret.add(linkTo(methodOn(UserClientsPasswordResource.class).changePassword(null, null, null)).withRel("changePassword"));
         }
         
         return ret;
