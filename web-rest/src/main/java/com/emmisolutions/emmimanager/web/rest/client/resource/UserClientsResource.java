80224deebd1f9bca9a0c172ea141daacf1aac499
Matt Fleming <7wjchnb836@snkmail.com>
2015-03-20 12:02:09 -0500

    Fix to not return entities on verify email token

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/resource/UserClientsResource.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/resource/UserClientsResource.java
@@ -1,47 +1,37 @@
 package com.emmisolutions.emmimanager.web.rest.client.resource;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
-
-import java.util.List;
-
-import javax.annotation.Resource;
-
+import com.emmisolutions.emmimanager.model.user.client.UserClient;
+import com.emmisolutions.emmimanager.service.UserClientService;
+import com.emmisolutions.emmimanager.service.UserClientService.UserClientConflict;
+import com.emmisolutions.emmimanager.service.UserClientValidationEmailService;
+import com.emmisolutions.emmimanager.service.mail.MailService;
+import com.emmisolutions.emmimanager.service.security.UserDetailsService;
+import com.emmisolutions.emmimanager.web.rest.admin.model.user.client.UserClientResourceAssembler;
 import com.emmisolutions.emmimanager.web.rest.admin.security.RootTokenBasedRememberMeServices;
-import org.apache.commons.lang3.StringUtils;
 import com.emmisolutions.emmimanager.web.rest.client.model.ValidationToken;
+import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserClientResourceAssembler;
+import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserClientValidationErrorResourceAssembler;
+import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserConflictResourceAssembler;
+import com.emmisolutions.emmimanager.web.rest.client.model.user.UserClientResource;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.hateoas.ResourceAssembler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.emmisolutions.emmimanager.model.user.client.UserClient;
-import com.emmisolutions.emmimanager.service.UserClientService;
-import com.emmisolutions.emmimanager.service.UserClientValidationEmailService;
-import com.emmisolutions.emmimanager.service.UserClientService.UserClientConflict;
-import com.emmisolutions.emmimanager.service.mail.MailService;
-import com.emmisolutions.emmimanager.service.security.UserDetailsService;
-import com.emmisolutions.emmimanager.web.rest.admin.model.user.client.UserClientResourceAssembler;
-import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserClientResourceAssembler;
-import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserConflictResourceAssembler;
-import com.emmisolutions.emmimanager.web.rest.client.model.user.ClientUserClientValidationErrorResourceAssembler;
-
-import com.emmisolutions.emmimanager.web.rest.client.model.user.UserClientResource;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.annotation.Resource;
 import javax.annotation.security.PermitAll;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.MediaType.APPLICATION_XML_VALUE;
 
 
 /**
@@ -144,10 +134,10 @@ public class UserClientsResource {
      */
     @RequestMapping(value = "/validate/", method = RequestMethod.PUT)
     @PermitAll
-    public ResponseEntity<UserClientResource> validateEmailToken(@RequestBody ValidationToken validationToken) {
+    public ResponseEntity<Void> validateEmailToken(@RequestBody ValidationToken validationToken) {
         UserClient savedUserClient = userClientValidationEmailService.validateEmailToken(validationToken.getValidationToken());
         if (savedUserClient != null) {
-            return new ResponseEntity<>(userResourceAssembler.toResource(savedUserClient), HttpStatus.OK);
+            return new ResponseEntity<>(HttpStatus.OK);
         }
         return new ResponseEntity<>(HttpStatus.GONE);
     }
