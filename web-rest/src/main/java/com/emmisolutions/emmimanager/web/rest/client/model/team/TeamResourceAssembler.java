222a8aef3055c5b5976aa2eb9a9e4841ac8d7c64
Matt Fleming <mfleming@emmisolutions.com>
2015-03-31 10:32:17 -0500

    EM-895 A client user can select a location
    Complete

--- web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/model/team/TeamResourceAssembler.java
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/client/model/team/TeamResourceAssembler.java
@@ -6,6 +6,8 @@ import com.emmisolutions.emmimanager.web.rest.client.resource.SchedulesResource;
 import org.springframework.hateoas.*;
 import org.springframework.stereotype.Component;
 
+import static com.emmisolutions.emmimanager.web.rest.client.resource.ProgramsResource.TEAM_LOCATION_ID_REQUEST_PARAM;
+import static com.emmisolutions.emmimanager.web.rest.client.resource.ProgramsResource.TEAM_PROVIDER_ID_REQUEST_PARAM;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
@@ -30,24 +32,27 @@ public class TeamResourceAssembler
                 .withRel("programs").getHref()), "programs"));
 
         ret.add(new Link(addPaginationTemplate(linkTo(methodOn(ProgramsResource.class)
-                .locations(entity.getClient().getId(), entity.getId(), null, null))
-                .withRel("locations").getHref()), "locations"));
+                .locations(entity.getClient().getId(), entity.getId(), null, null, null))
+                .withRel("locations").getHref()).with(
+                new TemplateVariables(new TemplateVariable(TEAM_PROVIDER_ID_REQUEST_PARAM,
+                        TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED))), "locations"));
 
         ret.add(new Link(addPaginationTemplate(linkTo(methodOn(ProgramsResource.class)
-                .providers(entity.getClient().getId(), entity.getId(), null, null))
-                .withRel("providers").getHref()), "providers"));
+                .providers(entity.getClient().getId(), entity.getId(), null, null, null))
+                .withRel("providers").getHref()).with(
+                new TemplateVariables(new TemplateVariable(TEAM_LOCATION_ID_REQUEST_PARAM,
+                        TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED))), "providers"));
 
         return ret;
     }
 
 
-    private UriTemplate addPaginationTemplate(String baseUri){
+    private UriTemplate addPaginationTemplate(String baseUri) {
         return new UriTemplate(baseUri)
                 .with(new TemplateVariables(
                         new TemplateVariable("page",
                                 TemplateVariable.VariableType.REQUEST_PARAM),
-                        new TemplateVariable(
-                                "sort",
+                        new TemplateVariable("sort",
                                 TemplateVariable.VariableType.REQUEST_PARAM_CONTINUED)));
     }
 }
