af2ce8f24029279727cd312e2ac3c36c554f94a6
Wendy Lee <wlee@emmisolutions.com>
2015-05-18 13:35:44 -0500

    EM-932, An emmi user can configure the "Email" field on scheduling for a given team

--- service-spring/src/test/java/com/emmisolutions/emmimanager/service/spring/ClientTeamEmailConfigurationServiceIntegrationTest.java
+++ service-spring/src/test/java/com/emmisolutions/emmimanager/service/spring/ClientTeamEmailConfigurationServiceIntegrationTest.java
@@ -12,19 +12,26 @@ import javax.annotation.Resource;
 import javax.transaction.Transactional;
 
 import org.apache.commons.lang3.RandomStringUtils;
+import org.joda.time.LocalDate;
 import org.junit.Test;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 
 import com.emmisolutions.emmimanager.model.Client;
+import com.emmisolutions.emmimanager.model.ClientRegion;
 import com.emmisolutions.emmimanager.model.ClientTeamEmailConfiguration;
+import com.emmisolutions.emmimanager.model.ClientTier;
+import com.emmisolutions.emmimanager.model.ClientType;
+import com.emmisolutions.emmimanager.model.SalesForce;
 import com.emmisolutions.emmimanager.model.Team;
 import com.emmisolutions.emmimanager.model.configuration.EmailRestrictConfiguration;
 import com.emmisolutions.emmimanager.model.configuration.team.DefaultClientTeamEmailConfiguration;
 import com.emmisolutions.emmimanager.persistence.DefaultClientTeamEmailConfigurationPersistence;
 import com.emmisolutions.emmimanager.service.BaseIntegrationTest;
+import com.emmisolutions.emmimanager.service.ClientService;
 import com.emmisolutions.emmimanager.service.ClientTeamEmailConfigurationService;
+import com.emmisolutions.emmimanager.service.TeamService;
 
 /**
  * An integration test for ClientTeamEmailConfigurationServiceImpl
@@ -37,6 +44,12 @@ public class ClientTeamEmailConfigurationServiceIntegrationTest extends
     
     @Resource
     DefaultClientTeamEmailConfigurationPersistence defaultClientTeamEmailConfigurationPersistence;
+    
+    @Resource
+    TeamService teamService;
+    
+    @Resource
+    ClientService clientService;
 
     /**
      * Test CRUD
@@ -90,6 +103,76 @@ public class ClientTeamEmailConfigurationServiceIntegrationTest extends
     }
     
     /**
+     * Test CRUD
+     */
+    @Test
+    public void testFind() {
+        Client client = new Client();
+        client.setTier(new ClientTier(2l));
+        client.setContractEnd(LocalDate.now().plusYears(1));
+        client.setContractStart(LocalDate.now());
+        client.setRegion(new ClientRegion(1l));
+        client.setName("Emmi Testing Email Configuration");
+        client.setType(new ClientType(2l));
+        client.setActive(true);
+        client.setContractOwner(makeNewRandomUserAdmin());
+        client.setSalesForceAccount(new SalesForce(RandomStringUtils
+                .randomAlphanumeric(18)));
+        clientService.create(client);
+        Team team = makeNewRandomTeam(client);
+                
+        Page<ClientTeamEmailConfiguration> listOfEmailConfig  = clientTeamEmailConfigurationService.findByClientIdAndTeamId(client.getId(), team.getId(), null);
+        
+        assertThat("should not contain email configuration",
+        		listOfEmailConfig.getContent().size() < 1, is(false));
+        
+        if(!listOfEmailConfig.hasContent()){
+        	Client reloadClient = clientService.reload(client);
+            Team reloadTeam = teamService.reload(team);
+            List<ClientTeamEmailConfiguration> list = new ArrayList<ClientTeamEmailConfiguration> ();
+            
+            Page<DefaultClientTeamEmailConfiguration> defaultEmailConfigList =  defaultClientTeamEmailConfigurationPersistence.findActive(new PageRequest(0, 10));
+            
+            DefaultClientTeamEmailConfiguration defaultEmailConfig = defaultEmailConfigList.getContent().get(0);
+            DefaultClientTeamEmailConfiguration defaultEmailConfigTwo = defaultEmailConfigList.getContent().get(1);
+
+            ClientTeamEmailConfiguration emailConfig = new ClientTeamEmailConfiguration();
+            emailConfig.setTeamEmailConfigurationId(defaultEmailConfig.getId());
+            emailConfig.setDescription(defaultEmailConfig.getDescription());
+            emailConfig.setClient(client);
+            emailConfig.setTeam(team);
+            
+            ClientTeamEmailConfiguration emailConfigTwo = new ClientTeamEmailConfiguration();
+            emailConfigTwo.setTeamEmailConfigurationId(defaultEmailConfigTwo.getId());
+            emailConfigTwo.setDescription(defaultEmailConfigTwo.getDescription());
+            emailConfigTwo.setClient(reloadClient);
+            emailConfigTwo.setTeam(reloadTeam);
+            
+            List<ClientTeamEmailConfiguration> emailConfigList = new ArrayList<ClientTeamEmailConfiguration>();
+            emailConfigList.add(emailConfig);
+            emailConfigList.add(emailConfigTwo);
+            
+            ClientTeamEmailConfiguration emailConfigSaved= clientTeamEmailConfigurationService.saveOrUpdate(emailConfig);
+            ClientTeamEmailConfiguration emailConfigTwoSaved= clientTeamEmailConfigurationService.saveOrUpdate(emailConfigTwo);
+            
+            ClientTeamEmailConfiguration reloadEmailConfig = clientTeamEmailConfigurationService.reload(listOfEmailConfig.getContent().get(0).getId());
+            
+            
+            assertThat("should contain email configuration",
+            		listOfEmailConfig.getContent(), hasItem(emailConfigSaved));
+            
+            assertThat("should contain eamil configuration",
+            		listOfEmailConfig.getContent(), hasItem(emailConfigTwoSaved));
+            				
+            assertThat("reload the same instance email configuration",
+            		reloadEmailConfig.getId(), is(emailConfigSaved.getId()));
+            
+        }
+ 
+    }
+    
+    
+    /**
      * Test bad Reload
      */
     @Test(expected = InvalidDataAccessApiUsageException.class)
@@ -97,5 +180,23 @@ public class ClientTeamEmailConfigurationServiceIntegrationTest extends
     	clientTeamEmailConfigurationService
                 .reload(null);
     }
+    
+    /**
+     * Test bad saveAndUpdate
+     */
+    @Test(expected = InvalidDataAccessApiUsageException.class)
+    public void testNegativeSaveOrUpdateNull() {
+    	clientTeamEmailConfigurationService.saveOrUpdate(null);
+    }
+    
+    
+    /**
+     * Test bad saveAndUpdate
+     */
+    @Test(expected = InvalidDataAccessApiUsageException.class)
+    public void testNegativeFindNull() {
+    	clientTeamEmailConfigurationService.findByClientIdAndTeamId(null, null, null);
+    }
+    
     
 }
