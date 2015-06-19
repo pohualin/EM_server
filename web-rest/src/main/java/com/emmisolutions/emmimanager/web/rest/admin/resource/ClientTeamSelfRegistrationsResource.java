4154893cd58fb88099e54333c6de3ce2f7b78cc7
Amrita Chavan <achavan@emmisolutions.com>
2015-06-19 14:23:40 -0500

    EM-714

---
+++ web-rest/src/main/java/com/emmisolutions/emmimanager/web/rest/admin/resource/ClientTeamSelfRegistrationsResource.java
@@ -0,0 +1,10 @@
+package com.emmisolutions.emmimanager.web.rest.admin.resource;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by achavan on 6/19/2015.
+ */
+@RestController
+public class ClientTeamSelfRegistrationsResource {
+}
