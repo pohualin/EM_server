57d26bb711decbb6e510333705c99c4ef78b2f58
Paul Lin <plin@emmisolutions.com>
2015-07-29 15:49:48 -0500

    Revert file

--- persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/ClientProviderPersistenceImpl.java
+++ persistence-jpa/src/main/java/com/emmisolutions/emmimanager/persistence/impl/ClientProviderPersistenceImpl.java
@@ -3,11 +3,9 @@ package com.emmisolutions.emmimanager.persistence.impl;
 import com.emmisolutions.emmimanager.model.ClientProvider;
 import com.emmisolutions.emmimanager.model.Provider;
 import com.emmisolutions.emmimanager.persistence.ClientProviderPersistence;
-import com.emmisolutions.emmimanager.persistence.impl.specification.ClientProviderSpecifications;
 import com.emmisolutions.emmimanager.persistence.repo.ClientProviderRepository;
 import com.emmisolutions.emmimanager.persistence.repo.ClientRepository;
 import com.emmisolutions.emmimanager.persistence.repo.ProviderRepository;
-
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,7 +15,6 @@ import org.springframework.stereotype.Repository;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,9 +32,6 @@ public class ClientProviderPersistenceImpl implements ClientProviderPersistence 
 
     @Resource
     ProviderRepository providerRepository;
-    
-    @Resource
-    ClientProviderSpecifications clientProviderSpecifications;
 
     @Override
     public Page<ClientProvider> findByClientId(Long clientId, Pageable pageable) {
@@ -45,7 +39,7 @@ public class ClientProviderPersistenceImpl implements ClientProviderPersistence 
             throw new InvalidDataAccessApiUsageException("Client Id cannot be null");
         }
         return clientProviderRepository.findByClientId(clientId,
-                pageable != null ? pageable : new PageRequest(0, 10, Sort.Direction.DESC, "specialty.name"));
+                pageable != null ? pageable : new PageRequest(0, 10, Sort.Direction.DESC, "createdDate"));
     }
     
 	@Override
